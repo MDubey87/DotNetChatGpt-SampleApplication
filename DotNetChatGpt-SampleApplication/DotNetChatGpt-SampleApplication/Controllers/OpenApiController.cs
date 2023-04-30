20fe4979e85bf8a934ee5613f814c7a1c4e48bc2
Manish Dubey <mandeep.mca2007@gmail.com>
2023-04-29 20:12:01 -0600

    Code to integrate the ChatGpt in .Net core Api.

---
+++ DotNetChatGpt-SampleApplication/DotNetChatGpt-SampleApplication/Controllers/OpenApiController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI_API;
+using OpenAI_API.Completions;
+
+namespace DotNetChatGpt_SampleApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpenApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("ChatGpt")]
+        public async Task<IActionResult>ChatGpt(string query)
+        {
+            string result=string.Empty;
+            var openAi = new OpenAIAPI("ApiKey"); // Pass your Api Key
+            CompletionRequest completionRequest=new CompletionRequest();
+            completionRequest.Prompt=query;
+            completionRequest.Model = OpenAI_API.Models.Model.DavinciText;
+            completionRequest.MaxTokens=1024;
+            var completions = await openAi.Completions.CreateCompletionAsync(completionRequest);
+            foreach(var completion in completions.Completions)
+            {
+                result += completion.Text;
+            }
+            return Ok(result);
+
+        }
+    }
+}
